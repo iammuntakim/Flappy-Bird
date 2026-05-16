e7ec9322c5606fe9df85d8f56bc5927877142d16
iammuntakim <mdmuntakimamin@gmail.com>
2026-05-16 21:17:31 +0600

    Flappy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,30 +1,30 @@
 plugins {
-	id 'com.android.application'
+    id("com.android.application") version "8.12.0"
 }
 
 android {
-	compileSdk 34
-	
-	defaultConfig {
-		applicationId "com.maplays.flappybird"
-		namespace "com.maplays.flappybird"
-		minSdkVersion 21
-		targetSdkVersion 34
-		versionCode 4
-		versionName "1.3"
-	}
-	
-	buildTypes {
-		release {
-			minifyEnabled false
-			proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
-		}
-	}
-	buildFeatures {
-		viewBinding false
-	}
+    compileSdk = 34
+    namespace = "com.maplays.flappybird"
+
+    defaultConfig {
+        applicationId = "com.maplays.flappybird"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 4
+        versionName = "1.3"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    buildFeatures {
+        viewBinding = false
+    }
 }
 
 dependencies {
-	implementation fileTree(dir: 'libs', include: ['*.jar'])
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 }
